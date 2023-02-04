14c37a9686eef381f9496431da6c9b3a3f6e7f89
Rger <mckeenweb@gmail.com>
2023-02-05 06:18:23 +0800

    test

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,9 +1,14 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200,{'content-type':'text/html'})
-  res.write('<h1>home page</h1>')
-  res.end()
+  // console.log(req.method)
+  const url = req.url;
+  if(url === '/'){
+    res.writeHead(200,{'content-type':'text/html'})
+    res.write('<h1>home page</h1>')
+    res.end()
+}
+  
 })
 
 server.listen(5000)
