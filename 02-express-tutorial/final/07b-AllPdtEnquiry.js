9e90cfc6e8b0c38f5ccbca9e9aea776a309f14e4
Rger <mckeenweb@gmail.com>
2023-02-15 06:21:51 +0800

    07c-SinglePdtEnquiry

--- 02-express-tutorial/final/07b-AllPdtEnquiry.js
+++ 02-express-tutorial/final/07b-AllPdtEnquiry.js
@@ -6,17 +6,11 @@ app.get('/', (req, res) => {
   res.send('<h1>Home Page</h1><a href="/api/products</a>')
 })
 app.get('/api/products',(req, res)=>{
-  const newProducts = products.map((product)=>{
-    const {id,name,image}= product;
-    return {id,name,image}
-  })
-  res.json(newProducts)
+const newProducts = products.map((product)=>{
+  const {id,name,image}= product;
+  return {id,name,image}
 })
-app.get('/api/products/1',(req, res)=>{
-const singleProduct = products.find((product)=> product.id ===1)
- 
-
-  res.json(singleProduct)
+res.json(newProducts)
 })
 app.listen(5000, () => {
   console.log('Server is listening on port 5000....')
