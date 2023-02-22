658c49dbb559fab90b043fe023cc3fe790484654
Rger <mckeenweb@gmail.com>
2023-02-23 07:40:23 +0800

    M file app.js

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,21 +1,24 @@
-const express = require('express')
-const logger = require('./logger')
+const express = required('express')
 const app = express()
+const logger = require('./logger')
+// const authorize = require('./authorize')
+//  req => middleware => res
 app.use(logger)
-
-
+// api/home/about/products
 app.get('/', (req, res) => {
   res.send('Home')
 })
 app.get('/about', (req, res) => {
   res.send('About')
 })
-app.get('/api/items', (req, res) => {
-  res.send('Items')
-})
 app.get('/api/products', (req, res) => {
   res.send('Products')
 })
+app.get('/api/items', (req, res) => {
+  console.log(req.user)
+  res.send('Items')
+})
+
 app.listen(5000, () => {
   console.log('Server is listening on port 5000....')
 })
