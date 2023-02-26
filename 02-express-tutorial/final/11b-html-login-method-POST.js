5d7cdcdebffb0a0009bd5b9cecb750101dc304aa
Rger <mckeenweb@gmail.com>
2023-02-26 20:17:33 +0800

    7:15 javascript method app js

--- 02-express-tutorial/final/11b-html-login-method-POST.js
+++ 02-express-tutorial/final/11b-html-login-method-POST.js
@@ -6,24 +6,20 @@ let { people } = require('./data')
 app.use(express.static('./methods-public'))
 // parse form data
 app.use(express.urlencoded({ extended: false }))
-
-// parse json
 app.get('/api/people', (req, res) => {
   res.status(200).json({ success: true, data: people })
  })
 
-app.post('/api/people', (req, res) => {
-  res.status(201).send('Success')
-})
-  // const { name } = req.body
-  // if (!name) {
-  //   return res
-  //     .status(400)
-  //     .json({ success: false, msg: 'please provide name value' })
-  // }
+// app.post('/api/people', (req, res) => {
+//   const { name } = req.body
+//   if (!name) {
+//     return res
+//       .status(400)
+//       .json({ success: false, msg: 'please provide name value' })
+//   }
 //   res.status(201).json({ success: true, person: name })
 // })
-  
+
 // app.post('/api/postman/people', (req, res) => {
 //   const { name } = req.body
 //   if (!name) {
