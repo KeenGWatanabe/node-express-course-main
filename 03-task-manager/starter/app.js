37f062c17e7291c2ab13839c804a1e98152491b8
Rger <mckeenweb@gmail.com>
2023-03-10 07:50:49 +0800

    1hr 15min connect MongoDB and gitignore done

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
-// const tasks = require('../controllers/tasks')
-
+const connectDB = require('./db/connect')
+require('dotenv').config()
 //middleware
+
 app.use(express.json())
 
 //routes
@@ -21,5 +22,14 @@ app.use('/api/v1/tasks', tasks)
 
 const port = 3000
 
-app.listen(port, console.log('Server is listening on port ${port}...'))
+const start = async ()=>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}...`))
 
+    } catch (error){
+        console.log(error)
+    }
+}
+
+start()
--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,2 +1,13 @@
-const connectionString = 
-'mongodb+srv://Rger:<password>@nodeexpressproject.abv8t0q.mongodb.net/?retryWrites=true&w=majority'
+const mongoose = require('mongoose')
+
+
+const connectDB = (url) => {
+return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+})
+}
+
+module.exports = connectDB
