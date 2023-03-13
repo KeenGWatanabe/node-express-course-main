afdbcb069d017464e47913201dec02794a9507f2
Rger <mckeenweb@gmail.com>
2023-03-14 06:28:45 +0800

    1:40 try catch added to controllers

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -4,8 +4,13 @@ const getAllTasks = (req, res) => {
 }
 
 const createTask = async (req, res) => {
+try{
     const task = await Task.create(req.body)
     res.status(201).json({ task })
+}
+catch (error) { 
+ res.status(500).json({msg:error})
+}    
 }
 const getTask = (req, res) => {
     res.json({id:req.params.id})
