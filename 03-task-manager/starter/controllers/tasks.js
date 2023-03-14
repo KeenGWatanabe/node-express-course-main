05ea83467e9245885af5b2419153e324eac96c11
Rger <mckeenweb@gmail.com>
2023-03-15 07:26:15 +0800

    1:56 GetSingleTask by ID

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -10,15 +10,23 @@ try{
   
 const createTask = async (req, res) => {
 try{
-  const task = await Task.create(req.body)
-  res.status(201).json({ task })
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
+    }catch (error) { 
+    res.status(500).json({msg:error})
+    }    
 }
-catch (error) { 
- res.status(500).json({msg:error})
-}    
+const getTask = async (req, res) => {
+try{
+const {id:taskID} = req.params
+const task = await Task.findOne({_id:taskID});
+if(!task){
+    return res.status(404).json({msg:`No task with id : ${taskID}`})
 }
-const getTask = (req, res) => {
-    res.json({id:req.params.id})
+res.status(200).json({ task })
+} catch (error) {
+    res.status(500).json({msg:error})
+}
 }
 const updateTask = (req, res) => {
     res.send('update task')
