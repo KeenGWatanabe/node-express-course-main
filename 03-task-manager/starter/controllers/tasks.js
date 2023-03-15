8b72202613ed0d87c01d812075999118f6e860c1
Rger <mckeenweb@gmail.com>
2023-03-16 06:42:02 +0800

    2:02 delete task blcok done

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -19,8 +19,8 @@ try{
 const getTask = async (req, res) => {
 try{
 const {id:taskID} = req.params
-const task = await Task.findOne({_id:taskID});
-if(!task){
+const task = await Task.findOne({_id: taskID});
+if (!task) {
     return res.status(404).json({msg:`No task with id : ${taskID}`})
 }
 res.status(200).json({ task })
@@ -31,8 +31,17 @@ res.status(200).json({ task })
 const updateTask = (req, res) => {
     res.send('update task')
 }
-const deleteTask = (req, res) => {
-    res.send('delete task')
+const deleteTask = async (req, res) => {
+try{
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({_id:taskID});
+    if (!task) {
+        return res.status(404).json({msg:`No task with id : ${taskID}`})
+    }
+res.status(200).json({task})
+} catch (error) {
+res.status(500).json({ msg: error })
+}
 }
 module.exports = {
     getAllTasks,
