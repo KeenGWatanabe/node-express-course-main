89a17c946ad631ad1d36adba05008dbff7d2e6a3
Rger <mckeenweb@gmail.com>
2023-04-01 08:52:08 +0800

    3:29 Postman GET req setup var

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -18,6 +18,7 @@ app.use(express.json())
 app.get('/',(req,res)=>{
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
 })
+
 app.use('/api/v1/products', productsRouter)
 // products route
 app.use(notFoundMiddleware)
