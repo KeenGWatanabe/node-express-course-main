ad2d4e033161376c4dffccb53a202278ca3a314c
Rger <mckeenweb@gmail.com>
2023-04-10 06:52:31 +0800

    4:39 select fields function added

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,11 +1,11 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('-name price')
+  const products = await Product.find({}).select('name price')
   res.status(200).json({ products, nbHits: products.length })
 }
 const getAllProducts = async (req, res) =>{
-  const { featured, company, name, sort } = req.query //key values used in query
+  const { featured, company, name, sort, fields } = req.query //key values used in query
   const queryObject = {}
 
   if (featured) {
@@ -20,11 +20,19 @@ const getAllProducts = async (req, res) =>{
 
   console.log(queryObject)
   let result = Product.find(queryObject)
+  //sort
   if(sort) {
     const sortList = sort.split(',').join('');
     result = result.sort(sortList)
   }else{
     result = result.sort('createAt')
+  }
+  //fields
+  if(fields) {
+    const fieldsList = fields.split(',').join('');
+    result = result.select(fieldsList)
+  }else{
+    result = result.select('createAt')
 
   }
   const products = await result
