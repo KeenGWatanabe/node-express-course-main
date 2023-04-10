c6cee5d7314681762446213e37c44f57560559b0
Rger <mckeenweb@gmail.com>
2023-04-11 05:40:33 +0800

    4:50 page and limit set

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,7 +1,11 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select('name price')
+  const products = await Product.find({})
+  .sort('name')
+  .select('name price')
+  .limit(10)
+  .skip(5)
   res.status(200).json({ products, nbHits: products.length })
 }
 const getAllProducts = async (req, res) =>{
@@ -33,8 +37,14 @@ const getAllProducts = async (req, res) =>{
     result = result.select(fieldsList)
   }else{
     result = result.select('createAt')
-
   }
+  const page = Number(req.query.page) || 1  //if page no. OR 1
+  const limit = Number(req.query.limit) || 10 //if limit OR 10
+  const skip = (page-1) * limit;
+
+  result = result.skip(skip).limit(limit)
+  // 23 pages if page stated eg , (2-1)*10=10
+
   const products = await result
   res.status(200).json({ products, nbHits: products.length })
 }
