a2eeb90e7e740f271cadb37fbc1e3b6da292ccae
Rger <mckeenweb@gmail.com>
2023-04-12 06:36:48 +0800

    5hr price operator setup

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,15 +1,14 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({})
+  const products = await Product.find({price:{ $gt: 50 }}) // $gt is >
   .sort('name')
-  .select('name price')
-  .limit(10)
-  .skip(5)
+  .select('name price') //.limit(10)//.skip(5)
+    
   res.status(200).json({ products, nbHits: products.length })
-}
+}  
 const getAllProducts = async (req, res) =>{
-  const { featured, company, name, sort, fields } = req.query //key values used in query
+  const { featured, company, name, sort, fields, numericFilters } = req.query //key values used in query
   const queryObject = {}
 
   if (featured) {
@@ -21,8 +20,24 @@ const getAllProducts = async (req, res) =>{
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' } 
   }
-
-  console.log(queryObject)
+  if (numericFilters) {
+    const operatorMap = {
+      '>' : '$gt', 
+      '>=' : '$gte',
+      '=' : '$eq',
+      '<' : '$lt',
+      '<=' : '$lte',
+    }
+    const regEx = /\b(>|>=|=|<|<=)\b/g
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    )
+    console.log(filters)
+    //queryObject.numericFilters = { $regex: numericFilters, $options: 'i' } 
+  }
+  
+ console.log(queryObject)
   let result = Product.find(queryObject)
   //sort
   if(sort) {
@@ -40,10 +55,10 @@ const getAllProducts = async (req, res) =>{
   }
   const page = Number(req.query.page) || 1  //if page no. OR 1
   const limit = Number(req.query.limit) || 10 //if limit OR 10 items per page
-  const skip = (page-1) * limit;  // eg page 3, (3-1)*10
+  const skip = (page-1) * limit;  // eg page 3, (3-1)*10 = 20 items
 
   result = result.skip(skip).limit(limit)
-  // 23 items 3 pages  if pages stated eg 2, (2-1)*10=10 shows page 2, 10items per page
+  //23 items 3pages if pages stated eg 3, (3-1)*10=20 shows page 3,23-10=3 items page3
 
   const products = await result
   res.status(200).json({ products, nbHits: products.length })
