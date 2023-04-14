22492a64e1cd19a915de00ec714c61be3fc819a3
Rger <mckeenweb@gmail.com>
2023-04-15 05:50:48 +0800

    5:29 creating JWT

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,6 +1,19 @@
+// chk username, password in pot(login) request
+// if exist create new JWT
+// send back to frontend
 
+// setup authentication so only the request with JWT can access the dashboard
+const CustomeAPIError = require('../errors/custom-error')
 
 const login = async (req, res) => {
+const {username,password} = req.body
+// mongoose validation
+// Joi pckg
+// check in the controller
+if(!username || !password){
+throw new CustomeAPIError('Please provide email and password', 400)
+}
+  console.log(username,password)
   res.send('Fake Login/Register/SignUp Route')
 }
 
