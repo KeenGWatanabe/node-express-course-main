cf81d84f1701ed1f40c38c9a83ed48f69ad9a132
Rger <mckeenweb@gmail.com>
2023-04-19 08:02:05 +0800

    6:00 fixing jwt

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -13,25 +13,40 @@ const {username,password} = req.body
 // check in the controller
 
 if(!username || !password){
-throw new CustomeAPIError('Please provide email and password', 400)
+  throw new CustomeAPIError('Please provide email and password', 401)
 }
 // for demo, normally provided by DB!!!
 const id = new Date().getDate()
 // $ npm install jsonwebtoken; try to keep payload small, better experience for user
 // just for demo, in production use long, complex, unguessable string value!!!!!
 
-
-
-const token = jwt.sign({id, username},process.env.JWT_SECRET,{expiresIn:'30d'})
+const token = jwt.sign({id, username},process.env.JWT_SECRET,{expiresIn:'30d',})
 
 res.status(200).json({msg:'user created', token})
 //console.log(username,password)
-  res.send('Fake Login/Register/SignUp Route')
-}
+}  
 
 const dashboard = async (req, res) => {
-  const luckyNumber = Math.floor(Math.random() * 100)
-  res.status(200).json({msg:`Hello, John Doe`,secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
+  //const authHeader = req.header.authorization;
+
+  if(!authHeader || !authHeader.startsWith('Bearer')){
+    throw new CustomeAPIError('No token', 401)
+  }
+  
+const token = authHeader.split(' ')[1]
+
+try {
+  const decoded = jwt.verify(token,process.env.JWT_SECRET);
+  
+  const luckyNumber = Math.floor(Math.random() * 100);
+
+  res.status(200).json({msg:`Hello, ${decoded.username}`,secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
+  
+} catch (error) {
+  throw new CustomeAPIError('Not authorized to access this route', 401)
+
+}
+
 }
 
 module.exports = {
