965d8fa38837a6f928eedcd5b8b578afb3e46fcb
Rger <mckeenweb@gmail.com>
2023-04-20 06:02:44 +0800

    6:03 dashboard fixed header's'

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -15,36 +15,33 @@ const {username,password} = req.body
 if(!username || !password){
   throw new CustomeAPIError('Please provide email and password', 401)
 }
+
 // for demo, normally provided by DB!!!
 const id = new Date().getDate()
+
+
 // $ npm install jsonwebtoken; try to keep payload small, better experience for user
 // just for demo, in production use long, complex, unguessable string value!!!!!
-
-const token = jwt.sign({id, username},process.env.JWT_SECRET,{expiresIn:'30d',})
-
+const token = jwt.sign({id, username},process.env.JWT_SECRET,{expiresIn:'30d'})
 res.status(200).json({msg:'user created', token})
-//console.log(username,password)
+
 }  
 
-const dashboard = async (req, res) => {
-  //const authHeader = req.header.authorization;
+const dashboard = async (req,res) =>{
+  const authHeader = req.headers.authorization;
 
-  if(!authHeader || !authHeader.startsWith('Bearer')){
+  if(!authHeader || !authHeader.startsWith('Bearer ')){
     throw new CustomeAPIError('No token', 401)
   }
   
 const token = authHeader.split(' ')[1]
-
 try {
   const decoded = jwt.verify(token,process.env.JWT_SECRET);
-  
-  const luckyNumber = Math.floor(Math.random() * 100);
-
+  console.log(decoded);
+  const luckyNumber = Math.floor(Math.random()*100);
   res.status(200).json({msg:`Hello, ${decoded.username}`,secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
-  
 } catch (error) {
   throw new CustomeAPIError('Not authorized to access this route', 401)
-
 }
 
 }
