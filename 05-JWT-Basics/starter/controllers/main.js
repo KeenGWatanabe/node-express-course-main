5b18e2443a1818b09208cc55282520ff08d5610d
Rger <mckeenweb@gmail.com>
2023-04-23 07:58:46 +0800

    6:28 refactor custom errors to clean up mainjs

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,24 +1,23 @@
 // chk username, password in pot(login) request
 // if exist create new JWT
 // send back to frontend
-
 // setup authentication so only the request with JWT can access the dashboard
+
 const jwt = require('jsonwebtoken')
-const CustomeAPIError = require('../errors/custom-error')
+const { BadRequestError } = require('../errors')
 
 const login = async (req, res) => {
-const {username,password} = req.body``
+const {username,password} = req.body
 // mongoose validation
 // Joi pckg
 // check in the controller
 
 if(!username || !password){
-  throw new CustomeAPIError('Please provide email and password', 401)
+  throw new BadRequestError ('Please provide email and password', 401)
 }
 
 // for demo, normally provided by DB!!!
 const id = new Date().getDate()
-
 
 // $ npm install jsonwebtoken; try to keep payload small, better experience for user
 // just for demo, in production use long, complex, unguessable string value!!!!!
@@ -28,22 +27,12 @@ res.status(200).json({msg:'user created', token})
 }  
 
 const dashboard = async (req,res) =>{
-  const authHeader = req.headers.authorization;
-
-  if(!authHeader || !authHeader.startsWith('Bearer ')){
-    throw new CustomeAPIError('No token', 401)
-  }
-  
-const token = authHeader.split(' ')[1]
-try {
-  const decoded = jwt.verify(token,process.env.JWT_SECRET);
-  console.log(decoded);
   const luckyNumber = Math.floor(Math.random()*100);
-  res.status(200).json({msg:`Hello, ${decoded.username}`,secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
-} catch (error) {
-  throw new CustomeAPIError('Not authorized to access this route', 401)
-}
 
+  res.status(200).json({
+    msg:`Hello, ${req.user.username}`,
+    secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
+    
 }
 
 module.exports = {
