a496f2e862f58fe709c7dd853123089bf4e780f1
KeenGWatanabe <mckeenweb@gmail.com>
2023-08-23 06:22:25 +0800

    timestamp in node 11-fs-async2

--- 01-node-tutorial/11-fs-async2.js
+++ 01-node-tutorial/11-fs-async2.js
@@ -1,4 +1,11 @@
+const { time, timeStamp } = require('console')
 const { readFile, writeFile } = require('fs')
+//writing timestamp in nodejs
+const timestamp = Date.now();
+const dateObject = new Date(timestamp);
+const date = dateObject.getDate();
+const month = dateObject.getMonth()+1; //Jan is 0, therefore month will always +1
+const year = dateObject.getFullYear();
 
 console.log('start')
 readFile('./content/first.txt', 'utf8', (err, result) => {
@@ -15,7 +22,7 @@ readFile('./content/first.txt', 'utf8', (err, result) => {
     const second = result
     writeFile(
       './content/result-async.txt',
-      `Lets change the script and see: ${first}`,
+      `Lets change the script and see: ${first}, overwritten by 11-fs-async2.js dated ${year}-${month}-${date}`,
       (err, result) => {
         if (err) {
           console.log(err)
