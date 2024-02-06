15d5318f1b9da0ab07f74eef433bb15d49b471ac
60936 <studio34pl@gmail.com>
2024-02-07 07:04:29 +0800

    r

--- 05-JWT-Basics/starter/app.js
+++ 05-JWT-Basics/starter/app.js
@@ -1,6 +1,8 @@
+// 1. load dotenv pkg
 require('dotenv').config();
 require('express-async-errors');
 
+// 2. imports express module to create application
 const express = require('express');
 const app = express();
 
@@ -8,15 +10,20 @@ const mainRouter = require('./routes/main');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-// middleware
+// 3. Middleware - serves static files (./public) 
 app.use(express.static('./public'));
+
+// parse(analyse) JSON payloads
 app.use(express.json());
 
-app.use('/api/v1',mainRouter); //to use login/dashboard, includes /api/v1
+//to use login/dashboard, includes /api/v1
+app.use('/api/v1',mainRouter); 
 
+// custome middleware error handling
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
+// load env var into process.env
 const port = process.env.PORT || 3000;
 
 const start = async () => {
@@ -29,4 +36,5 @@ const start = async () => {
   }
 };
 
+// invokes start fn
 start();
