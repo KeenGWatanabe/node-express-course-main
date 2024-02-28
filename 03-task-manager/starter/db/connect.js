1c6c92da5371be31fc378aa7546492624a559256
60936 <studio34pl@gmail.com>
2024-02-29 07:41:51 +0800

    r

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,6 +1,7 @@
+const connectionString = "mongodb+srv://Rger:<asdf>@atlascluster.e1g1exp.mongodb.net/"
 const mongoose = require('mongoose')
 
-
+// need to put your mongoDB url here.
 const connectDB = (url) => {
 return mongoose.connect(url, {
     useNewUrlParser: true,
