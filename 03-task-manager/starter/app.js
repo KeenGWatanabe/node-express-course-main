3d09b6a5fa90f7d4d4ec6c617cecdd800e27a213
60936 <studio34pl@gmail.com>
2024-03-02 15:55:40 +0800

    R

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -19,7 +19,7 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 const start = async ()=>{
     try{
-        await connectDB()
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server is listening on port ${port}...`)
         );
     } catch (error){
--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose')
 
-const connectionString = "mongodb+srv://rger:asdf@nodeexpressprojects.ttabe7j.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority&appName=NodeExpressProjects"
-
 // need to put your mongoDB url here.
 const connectDB = (url) => {
-return mongoose.connect(connectionString, {
+return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
