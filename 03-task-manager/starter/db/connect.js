933ae61d0cf754abb97c2985eb55ea7e6d13963e
60936 <studio34pl@gmail.com>
2024-03-02 08:32:19 +0800

    r

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,14 +1,15 @@
-const connectionString = "mongodb+srv://Rger:<asdf>@atlascluster.e1g1exp.mongodb.net/"
 const mongoose = require('mongoose')
+
+const connectionString = "mongodb+srv://rger:asdf@nodeexpressprojects.ttabe7j.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority&appName=NodeExpressProjects"
 
 // need to put your mongoDB url here.
 const connectDB = (url) => {
-return mongoose.connect(url, {
+return mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-})
+}).then(() => console.log('connected to the DB...')).catch((err)=>console.log(err))
 }
 
 module.exports = connectDB
